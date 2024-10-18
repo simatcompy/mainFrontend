e7a1140a6ffdface3528c1bac2f1536386bcc418
TheZombie272 <uzumakyinuyasha@gmail.com>
2024-10-18 10:19:58 -0500

    organición de carpetas

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+// Función para cargar contenido HTML desde un archivo externo
+function cargarContenidoHTML(ruta, elementoID) {
+    fetch(ruta)  // Hacemos la solicitud al archivo HTML.
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al cargar el archivo HTML');
+            }
+            return response.text();  // Extraemos el texto (contenido HTML).
+        })
+        .then(data => {
+            // Insertamos el contenido en el elemento con el ID especificado.
+            document.getElementById(elementoID).innerHTML = data;
+        })
+        .catch(error => console.error('Error:', error));  // Manejamos errores.
+}
+
+// Cargar el contenido HTML en el div con id="contenido"
+//cargarContenidoHTML('assets/html/header.html', 'index-header');
+cargarContenidoHTML('assets/html/index-content.html', 'index-content');
+cargarContenidoHTML('assets/html/footer.html', 'index-footer');
